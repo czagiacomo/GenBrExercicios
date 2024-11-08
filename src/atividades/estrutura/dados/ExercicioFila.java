469908f99abccbae6cbff7074b513c0473d50a53
Catharina Zagiacomo <catharina.zagiacomo@gmail.com>
2024-11-07 21:07:14 -0300

    Exercicios de Estrutura de dados - Aula  11

--- src/atividades/estrutura/dados/ExercicioFila.java
+++ src/atividades/estrutura/dados/ExercicioFila.java
@@ -1,10 +1,78 @@
 package atividades.estrutura.dados;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
 public class ExercicioFila {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner scan = new Scanner(System.in);
+		Queue<String> fila = new LinkedList<String>();
+
+		int opcao;
+		
+		menu();
+
+		do {
+
+			System.out.print("Escolha uma opção: ");
+			opcao = scan.nextInt();
+
+			switch (opcao) {
+
+			case 1:
+				System.out.println("Digite o nome do cliente: ");
+				String pessoa = scan.next();
+				fila.add(pessoa);
+				lista(fila);
+				break;
+
+			case 2:
+				lista(fila);
+				break;
+
+			case 3:
+				if (fila.isEmpty()) {
+					System.out.println("A fila está vazia");
+
+				} else {
+					fila.remove();
+					lista(fila);
+					System.out.println("Cliente removido!");
+				}
+				break;
+
+			case 0:
+				System.out.println("Finalizando o programa...");
+				break;
+
+			default:
+				System.out.println("Opção Inválida");
+				break;
+			}
+
+		} while (opcao != 0);
+
+		scan.close();
+
+	}
+
+	public static void menu() {
+
+		System.out.println("\n********** Menu **********");
+		System.out.println("1: Adicionar cliente na fila");
+		System.out.println("2: Listar todos os Clientes");
+		System.out.println("3: Retirar cliente da fila");
+		System.out.println("0: Sair");
+		System.out.println("********************");
+	}
+
+	public static void lista(Queue<String> fila) {
+		for (String pessoa : fila) {
+			System.out.println(pessoa);
+		}
 	}
 
 }
--- src/atividades/estrutura/dados/ExercicioPilha.java
+++ src/atividades/estrutura/dados/ExercicioPilha.java
@@ -1,20 +1,78 @@
 package atividades.estrutura.dados;
 
+import java.util.Collections;
+import java.util.Scanner;
+import java.util.Stack;
+
 public class ExercicioPilha {
 
 	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		Stack<String> pilha = new Stack<String>();
+
+		int opcao;
 		
+		menu();
+
+		do {
+
+			System.out.print("Escolha uma opção: ");
+			opcao = scan.nextInt();
+
+			switch (opcao) {
+
+			case 1:
+				System.out.println("Digite o nome do livro: ");
+				String livro = scan.next();
+				pilha.add(livro);
+				lista(pilha);
+				break;
+
+			case 2:
+				lista(pilha);
+				break;
+
+			case 3:
+				if (pilha.isEmpty()) {
+					System.out.println("A pilha está vazia");
+
+				} else {
+					pilha.pop();
+					lista(pilha);
+					System.out.println("Livro removido!");
+				}
+				break;
+
+			case 0:
+				System.out.println("Finalizando o programa...");
+				break;
+
+			default:
+				System.out.println("Opção Inválida");
+				break;
+			}
+
+		} while (opcao != 0);
+
+		scan.close();
 
 	}
-	
-	public static void opcoes() {
-	
-	}
-	
+
 	public static void menu() {
-		System.out.println("==============================");
-		System.out.println();
-		System.out.println("==============================");
+
+		System.out.println("\n********** Menu **********");
+		System.out.println("1: Adicionar livro na pilha");
+		System.out.println("2: Listar todos os livros");
+		System.out.println("3: Retirar livros da pilha");
+		System.out.println("0: Sair");
+		System.out.println("********************");
+	}
+
+	public static void lista(Stack<String> pilha) {
+		for (String livro : pilha) {
+			System.out.println(livro);
+		}
 	}
 
 }
